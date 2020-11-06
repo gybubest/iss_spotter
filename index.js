9811e81c3abbb8607f60226b73a5f2dc5f351121
Yu Gao <gaoyububest@gmail.com>
2020-11-06 02:45:08 +0000

    modified

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const { nextISSTimesForMyLocation } = require('./iss');
 nextISSTimesForMyLocation((error, passTimes) => {
   const printPassTimes = function(passTimes) {
     for (const pass of passTimes) {
-      const datetime = new Date(0);
-      datetime.setUTCSeconds(pass.risetime);
+      const datetime = new Date(pass.risetime * 1000);
       const duration = pass.duration;
       console.log(`Next pass at ${datetime} for ${duration} seconds!`);
     }
